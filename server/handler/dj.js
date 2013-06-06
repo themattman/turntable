c45151795b7ba0b6731d5108f5a160af9bc41cbd
Matt Kneiser <mattman@umich.edu>
2013-06-06 01:16:24 -0700

    Extra dj logging

--- server/handler/dj.js
+++ server/handler/dj.js
@@ -12,5 +12,7 @@ exports.dj = function(data, bot){
   if(mods.indexOf(data.user[0].userid) === -1){
     console.log('The DJ "%s" is not approved, he has been booted.', data.user[0].userid);
     bot.remDj(data.user[0].userid);
+  }else{
+    console.log('The DJ "%s" is approved, he is now on stage.', data.user[0].userid);
   }
 };
